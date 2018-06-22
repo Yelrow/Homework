606d948866742d78ad286202daa7008a35be3f6a
Christopher Worley <christopher.ccw@gmail.com>
2018-06-22 08:00:57 -0700

    W1D5 homework added

---
+++ W1D5/stack.rb
@@ -0,0 +1,25 @@
+class Stack
+    attr_accessor :stack
+
+    def initialize
+      @stack = []
+    end
+
+    def push(el)
+      # adds an element to the stack
+      stack.push(el)
+    end
+
+    def pop
+      # removes one element from the stack
+      stack.pop
+    end
+
+    def peek
+      # returns, but doesn't remove, the top element in the stack
+      stack.last
+    end
+  end
+
+  Stack.new
+  p stack.push(5)
